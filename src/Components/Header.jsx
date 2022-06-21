03d246dee2ce410261275583c805ddf88efe6a73
Cristiano Vieira <cristianoviieira@gmail.com>
2022-06-21 16:27:04 -0300

    adiciona botão que redireciona para a tela de perfil e outro que abre o filtro para pesquisa de receitas

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const [filter, setFilter] = useState('');
+  const [search, setSearch] = useState(false);
+
+  const handleFilter = ({ target }) => {
+    const { value } = target;
+    setFilter(value);
+  };
+  const buttonSearch = () => {
+    if (search) {
+      setSearch(false);
+    } else {
+      setSearch(true);
+    }
+  };
+
   return (
     <header>
-      <img src={ profileIcon } alt="profile icon" />
+      <Link to="/profile">
+        <img src={ profileIcon } alt="profile icon" />
+      </Link>
       <h1>
         TrybeTeste
       </h1>
-      <img src={ searchIcon } alt="profile icon" />
+      <div>
+        <button type="button" onClick={ () => buttonSearch() }>
+          <img src={ searchIcon } alt="profile icon" />
+        </button>
+        {search && (<input
+          type="text"
+          name=""
+          id=""
+          value={ filter }
+          onChange={ handleFilter }
+        />)}
+      </div>
     </header>
   );
 }
