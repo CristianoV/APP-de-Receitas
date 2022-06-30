de3ee929a87c91b2cf2ccb0387c36405bfcdfbde
Cristiano Vieira <cristianoviieira@gmail.com>
2022-06-30 00:40:20 -0300

    refatorando codigo

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -9,7 +9,14 @@ function DoneRecipes() {
       <div>
         <button data-testid="filter-by-all-btn" type="button">All</button>
         <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+        <button
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setRecipes('teste') }
+          type="button"
+        >
+          Drinks
+
+        </button>
       </div>
       <div>
         {recipes.map((recipe, index) => (
